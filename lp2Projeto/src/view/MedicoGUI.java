27b3f84233b8508186a775967a5316f1dd6d7fec
Raphael de Almeida Silva <rj.raphaelalmeida@gmail.com>
2022-09-21 22:15:50 -0300

    minor adjusts

--- lp2Projeto/src/view/MedicoGUI.java
+++ lp2Projeto/src/view/MedicoGUI.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,20 +13,20 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.EventQueue;
-import javax.swing.*;
+import javax.swing.JComboBox;
 
 
 
 public class MedicoGUI extends JFrame {
     private JPanel contentPane;
-    private JComboBox ComboBox;
     private JTextField txtTelefone;
     private JTextField txtCRM;
     private JTextField txtNome;
     private JTextField txtCPF;
-
     private JButton btnEnviar;
     private JButton btnLimpar;
+    public String especialidade[] = {"Cardiologista", "Clinico Geral", "Dermatologista", "Endocrinologista", "Neurologista"};
+    private JComboBox<String> cbBox;
     private static MedicoGUI frame;
 
     /**
@@ -47,29 +49,11 @@ public class MedicoGUI extends JFrame {
      * Crate the frame
      */
 
-
-    public abstract class ComboBox {
-        JFrame f;
-        ComboBox() {
-            f = new JFrame("Especialidade");
-            String especialidade[] = {
-                "Cardiologista", "Clinico Geral", "Dermatologista",
-                "Endocrinologista", "Neurologista"
-            };
-            JComboBox cb = new JComboBox(especialidade);
-            cb.setBounds(90, 240, 170, 25);
-            f.add(cb);
-            f.setLayout(null);
-            f.setSize(400, 500);
-            f.setVisible(true);
-        }
-    }
-
     public MedicoGUI(){
 
         setTitle("Cadastro de M" + '\u00E9' + "dicos");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(800, 800, 400, 400);
+        setBounds(800, 800, 400, 500);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5,5,5,5));
         setContentPane(contentPane);
@@ -108,15 +92,28 @@ public class MedicoGUI extends JFrame {
         contentPane.add(txtCPF);
         txtCPF.setColumns(10);
 
+        JLabel lblEspecialidade = new JLabel("Especialidade");
+        lblEspecialidade.setBounds(90, 230, 170, 25);
+        lblEspecialidade.setFont(new Font("Tahoma",Font.BOLD, 19));
+        contentPane.add(lblEspecialidade);
+
+        
+        cbBox = new JComboBox<>();
+        cbBox.setFont(new Font("Tahoma", Font.PLAIN, 19));
+        cbBox.setModel(new DefaultComboBoxModel<>(new String[] {"Cardiologista", "Clinico Geral", "Dermatologista", "Endocrinologista", "Neurologista"}));
+        cbBox.setSelectedIndex(0);
+        cbBox.setBounds(90, 270, 170, 25);
+        getContentPane().add(cbBox);
+      
 
         JLabel lblTelefone = new JLabel("Telefone");
         lblTelefone.setFont(new Font("Tahoma", Font.BOLD, 17));
-        lblTelefone.setBounds(90, 270, 100, 40);
+        lblTelefone.setBounds(90, 310, 100, 25);
         contentPane.add(lblTelefone);
 
         txtTelefone = new JTextField();
         txtTelefone.setFont(new Font("Tahoma", Font.PLAIN, 19));
-        txtTelefone.setBounds(90, 300, 170, 25);
+        txtTelefone.setBounds(90, 340, 170, 25);
         contentPane.add(txtTelefone);
         txtTelefone.setColumns(10);
 
@@ -130,7 +127,7 @@ public class MedicoGUI extends JFrame {
         }
         });
         btnLimpar.setFont(new Font("Tahoma", Font.BOLD, 17));
-        btnLimpar.setBounds(64, 310, 110, 32);
+        btnLimpar.setBounds(64, 390, 110, 32);
         contentPane.add(btnLimpar);
         
         btnEnviar = new JButton("Enviar");
@@ -149,8 +146,7 @@ public class MedicoGUI extends JFrame {
             }
         });
         btnEnviar.setFont(new Font("Tahoma", Font.BOLD, 17));
-        btnEnviar.setBounds(180, 310, 110, 32);
+        btnEnviar.setBounds(180, 390, 110, 32);
         contentPane.add(btnEnviar);
     }
-    
 }
